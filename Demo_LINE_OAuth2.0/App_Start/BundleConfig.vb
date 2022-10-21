c5387e7c91e15bcf9c59dabae352517709b68ad8
Paul <23330008+Sunichiyi@users.noreply.github.com>
2022-10-21 15:10:23 +0800

    新增 套件與圖片

---
+++ Demo_LINE_OAuth2.0/App_Start/BundleConfig.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Optimization
+
+Public Class BundleConfig
+    ' 如需統合的詳細資訊，請前往 https://go.microsoft.com/fwlink/?LinkID=303951
+    Public Shared Sub RegisterBundles(ByVal bundles As BundleCollection)
+        bundles.Add(New Bundle("~/bundles/css").Include(
+                     "~/Content/bootstrap.min.css",
+                                "~/css/sticky-footer-navbar.css"))
+
+        BundleTable.EnableOptimizations = True
+    End Sub
+End Class
---
+++ Demo_LINE_OAuth2.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Demo_LINE_OAuth2._0.Global_asax" Language="vb" %>
